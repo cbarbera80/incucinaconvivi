7fbab9174f44bab06bc4346b5b71bd54c724ab8f
Claudio Barbera <barbera.claudio@gmail.com>
2013-09-20 11:47:26 +0200

    modified some parameter for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Incucinaconvivi::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -28,14 +28,6 @@ Incucinaconvivi::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com" ,
-      :port => 587,
-      :domain => "incucinaconvivi.com" ,
-      :authentication => "plain" ,
-      :user_name => "barbera.claudio@gmail.com" ,
-      :password => "san.francisco" ,
-      :enable_starttls_auto => true
-  }
+
 
 end
